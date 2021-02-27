9491dc7cc3a99c17c53c11c85472e0313b267316
aryansahu2891 <75078190+aryansahu2891@users.noreply.github.com>
2021-02-27 20:10:43 +0530

    Add files via upload

---
+++ code!!!!.py
@@ -0,0 +1,16 @@
+x = 'mississippi'
+M = "m"
+I = "i"
+S = "s"
+P = "p"
+
+m = x.count(M)
+i = x.count(I)
+s = x.count(S)
+p = x.count(P)
+
+
+print("m =", m)
+print("i =", i)
+print("s =", s)
+print("p =", p)
